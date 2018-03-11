c2267531fcd9a48ee35c878205a31637341dee2c
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-11 14:56:34 +0100

    New test
    Include new test and see patching with return_value
    
    S3,L44

--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -11,3 +11,15 @@ class AppTest(TestCase):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
+
+    def test_menu_print_prompts(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_menu_calls_print_blogs(self):
+        with patch('app.print_blogs') as mocked_print_blogs:
+            with patch('builtins.input', return_value='q'):
+                app.menu()
+                mocked_input.assert_called_with(app.MENU_PROMPT)
+                mocked_print_blogs.assert_called_with()
